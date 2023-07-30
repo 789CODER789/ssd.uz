bdb4504db2ca92ffd7c6ad2a9c1af4990f95765e
Fazliddin <130275734+789CODER789@users.noreply.github.com>
2023-07-30 15:48:58 +0500

    Add files via upload

---
+++ ssd_portfolio.js
@@ -0,0 +1,43 @@
+$('.diva').click(function(){
+    $('#div-a').show(2000)
+    $('#div-b').hide()
+    $('#div-c').hide()
+    $('#div-d').hide()
+    $('#div-e').hide()
+})
+
+$('.divb').click(function(){
+    $('#div-a').hide()
+    $('#div-b').show(2000)
+    $('#div-c').hide()
+    $('#div-d').hide()
+    $('#div-e').hide()
+
+})
+
+$('.divc').click(function(){
+    $('#div-a').hide()
+    $('#div-b').hide()
+    $('#div-c').show(2000)
+    $('#div-d').hide()
+    $('#div-e').hide()
+
+})
+
+$('.divd').click(function(){
+    $('#div-a').hide()
+    $('#div-b').hide()
+    $('#div-c').hide()
+    $('#div-d').show(2000)
+    $('#div-e').hide()
+
+})
+
+$('.dive').click(function(){
+    $('#div-a').hide()
+    $('#div-b').hide()
+    $('#div-c').hide()
+    $('#div-d').hide()
+    $('#div-e').show(2000)
+
+})
